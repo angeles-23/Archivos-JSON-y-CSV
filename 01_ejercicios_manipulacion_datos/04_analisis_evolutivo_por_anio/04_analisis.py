a07e2691a07232bb7b39564aed861fe64efcc3ac
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-04-04 10:04:21 +0200

    Add files via upload

--- 01_ejercicios_manipulacion_datos/04_analisis_evolutivo_por_anio/04_analisis.py
+++ 01_ejercicios_manipulacion_datos/04_analisis_evolutivo_por_anio/04_analisis.py
@@ -3,21 +3,36 @@ os.system('cls')
 
 
 
-def cargar_csv(archivo_csv):
+def cargar_csv():
 
-    with open(archivo_csv, 'r', newline='', encoding='utf-8') as f:
+    with open('jobs_in_data.csv', 'r', newline='', encoding='utf-8') as f:
         datos = csv.DictReader(f)
         lista_datos = list(datos)
 
     return lista_datos
 
 
-def agrupar_por_anios(lista_datos_csv):
+def conocer_anios():
+    cargar_datos = cargar_csv()
+    lista_anios = []
+
+    for empleo in cargar_datos:
+        anios = int(empleo['work_year'])
+
+        if anios not in lista_anios:
+            lista_anios.append(anios)
+
+    return lista_anios
+
+
+def agrupar_por_anios():
+
+    cargar_datos = cargar_csv()
 
     diccionario_anios = {}
     lista_anios = []
 
-    for empleo in lista_datos_csv:
+    for empleo in cargar_datos:
         anio = int(empleo['work_year'])
         
         if anio not in lista_anios:
@@ -27,7 +42,7 @@ def agrupar_por_anios(lista_datos_csv):
     for dato_lista in lista_anios:
         empleos_por_anio = []
 
-        for linea in lista_datos_csv:
+        for linea in cargar_datos:
             anio = int(linea['work_year'])
 
             if dato_lista == anio:
@@ -38,9 +53,9 @@ def agrupar_por_anios(lista_datos_csv):
     return diccionario_anios
 
 
-def calcular_numero_total_empleos(archivo):
+def calcular_numero_total_empleos():
 
-    lista_datos_csv = cargar_csv(archivo)
+    lista_datos_csv = cargar_csv()
     cantidad_empleados = {}
 
     for empleo in lista_datos_csv:
@@ -54,9 +69,9 @@ def calcular_numero_total_empleos(archivo):
     return cantidad_empleados
 
 
-def calcular_salario_total_empleos(archivo):
+def calcular_salario_total_empleos():
 
-    lista_datos_csv = cargar_csv(archivo)
+    lista_datos_csv = cargar_csv()
     salarios_medios = {}
 
     for empleo in lista_datos_csv:
@@ -71,23 +86,79 @@ def calcular_salario_total_empleos(archivo):
     return salarios_medios
 
 
-def calcular_salario_medio(archivo):
+def calcular_salario_medio():
 
-    lista_datos_csv = cargar_csv(archivo)
-    cantidades_totales_salario = calcular_salario_total_empleos(lista_datos_csv)
-    cantidades_totales_empleos = calcular_numero_total_empleos(lista_datos_csv)
+    lista_datos_csv = cargar_csv()
+    cantidades_totales_salarios = calcular_salario_total_empleos()
+    cantidades_totales_empleos = calcular_numero_total_empleos()
 
     salarios_medios = {}
 
-    for empleo in cantidades_totales_empleos:
-        for salario in cantidades_totales_salario:
-            salarios = ...
+    for salarios in cantidades_totales_salarios:
+        for empleos in cantidades_totales_empleos:
+            if salarios == empleos:
+                salario_medio = cantidades_totales_salarios[salarios] / cantidades_totales_empleos[empleos]
+                salarios_medios[salarios] = round(salario_medio, 2)
+                
+    return salarios_medios
+
+
+def calcular_distribucion_employment_type():
+    cargar_datos = cargar_csv()
+    lista_anios = conocer_anios()
+    distribuciones_tipo = {}
+        
+    for empleo in cargar_datos:
+        for anios in lista_anios:
+            anio_empleo = int(empleo['work_year'])
+            tipo_trabajo = empleo['employment_type']
+
+            if anio_empleo == anios:
+                if anio_empleo not in distribuciones_tipo:
+                    distribuciones_tipo[anio_empleo] = {}
+                
+                if tipo_trabajo not in distribuciones_tipo[anio_empleo]:
+                    distribuciones_tipo[anio_empleo][tipo_trabajo] = 1
+                else:
+                    distribuciones_tipo[anio_empleo][tipo_trabajo] += 1
+            
+    return distribuciones_tipo            
+    
+    
+def calcular_dictribucion_work_setting():
+    cargar_datos = cargar_csv()
+    lista_anios = conocer_anios()
+    distribucion_tipos = {}
+
+    for empleo in cargar_datos:
+        for anios in lista_anios:
+
+            anio_empleo = int(empleo['work_year'])
+            entorno_empleo = empleo['work_setting']
+
+            if anio_empleo == anios:
+                if anio_empleo not in distribucion_tipos:
+                    distribucion_tipos[anio_empleo] = {}
+
+                if entorno_empleo not in distribucion_tipos[anio_empleo]:
+                    distribucion_tipos[anio_empleo][entorno_empleo] = 1
+                else:
+                    distribucion_tipos[anio_empleo][entorno_empleo] += 1
+    
+    return distribucion_tipos
+
+
+def guardar_resultados_csv():
+    ...
+
 
 
 
 if __name__ == '__main__':
-    archivo_csv = 'jobs_in_data.csv'
-    lista_datos_csv = cargar_csv(archivo_csv)
-    # print(agrupar_por_anios(lista_datos_csv))
-    print(calcular_numero_total_empleos(archivo_csv))
-    print(calcular_salario_total_empleos(archivo_csv))
+    # print(conocer_anios())
+    # print(agrupar_por_anios())
+    # print(calcular_numero_total_empleos())
+    # print(calcular_salario_total_empleos())
+    # print(calcular_salario_medio())
+    # print(calcular_distribucion_employment_type())
+    print(calcular_dictribucion_work_setting())
