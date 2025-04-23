c551454548aabeda5a7f08ebeac2329fcbc9bcb9
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-04-23 14:27:50 +0200

    Add files via upload

--- 01_ejercicios_manipulacion_datos/06_ejercicios_repaso/01_manipulacion_CSV.py
+++ 01_ejercicios_manipulacion_datos/06_ejercicios_repaso/01_manipulacion_CSV.py
@@ -1,10 +1,13 @@
-import csv, os
+import csv, json, os
 os.system('cls')
 
 
 def cargar_y_leer_datos_csv():
+    '''
+    Carga el archivo CSV y lee los datos.
+    '''
     with open('DATA.csv', mode='r', newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f, quotechar='"')   # 
+        reader = csv.DictReader(f, quotechar='"')    
         datos = list(reader)
 
         for linea in reader:
@@ -14,6 +17,9 @@ def cargar_y_leer_datos_csv():
 
 
 def cambiar_titulos_a_mayuscula():
+    '''
+    Transforma los títulos de los videojuegos a mayúsculas utilizando map().
+    '''
     datos = cargar_y_leer_datos_csv()
 
     # SOLO TITULOS EN MAYÚSCULA
@@ -26,12 +32,15 @@ def cambiar_titulos_a_mayuscula():
 
 
 def encontrar_videojuego_con_puntuacion_mayor_9():
+    '''
+    Filtra solo los videojuegos que tengan una puntuación de crítica superior a 9.0 usando filter()
+    '''
     datos = cargar_y_leer_datos_csv()
-
-
+    puntuacion_mayor_9 = list( filter( lambda videojuego:  float(videojuego['critic_score']) > 9.0, datos) )
+    return puntuacion_mayor_9
 
 
 if __name__ == '__main__':
     # print(cargar_y_leer_datos_csv())
-    print(cambiar_titulos_a_mayuscula())
-
+    # print(cambiar_titulos_a_mayuscula())
+    print(encontrar_videojuego_con_puntuacion_mayor_9())
--- 01_ejercicios_manipulacion_datos/06_ejercicios_repaso/02_manipulacion_critic_score.py
+++ 01_ejercicios_manipulacion_datos/06_ejercicios_repaso/02_manipulacion_critic_score.py
@@ -1,1 +1,28 @@
-import os, csv
+import os, csv, functools
+os.system('cls')
+
+
+def cargar_datos():
+    '''
+    Carga el archivo CSV y lee los datos.
+    '''
+    with open('DATA.csv', mode='r', newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f, quotechar='"')
+        datos = list(reader)
+
+        for linea in reader:
+            print(linea)
+    
+    return datos
+        
+
+
+def convertir_puntuaciones_a_enteros():
+    datos = cargar_datos()
+    return datos
+
+
+
+
+if __name__ == '__main__':
+    print(convertir_puntuaciones_a_enteros())
