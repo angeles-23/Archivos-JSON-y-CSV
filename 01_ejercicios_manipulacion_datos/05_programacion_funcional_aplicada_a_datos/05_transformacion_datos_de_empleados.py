4c986cebdbc90d5f419173dc01c74b117decb033
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-04-23 09:18:55 +0200

    Add files via upload

--- 01_ejercicios_manipulacion_datos/05_programacion_funcional_aplicada_a_datos/05_transformacion_datos_de_empleados.py
+++ 01_ejercicios_manipulacion_datos/05_programacion_funcional_aplicada_a_datos/05_transformacion_datos_de_empleados.py
@@ -3,23 +3,53 @@ os.system('cls')
 
 
 def cargar_datos_sin_librerias():
+    '''
+    Carga los datos manualmente (sin librerías).
+    
+    '''
     with open('empleados.csv', 'r') as f:
-        ...
+        lineas = f.readlines()
+        cabecera = lineas[0].strip().split(',')
+        datos = [ dict (zip (cabecera, linea.strip().split(',') ) )   for linea in lineas[1:] ]
+        
+
+    return datos
 
 
 
 def filtrar_empleados_activos():
-    ...
+    '''
+    Filtra los empleados activos.
+    '''
+    datos = cargar_datos_sin_librerias()
+    empleados_activos = list (filter (lambda empleado:empleado['activo'] == 'True', datos) ) 
+    return empleados_activos
+
 
 
 def aumentar_salario_10_porciento():
-    ...
+    '''
+    Aumenta su salario un 10% usando map.
+    '''
+    datos = cargar_datos_sin_librerias()
+    aumentar_salario_10_porciento = (list( map( lambda empleados: int(empleados['salario']) + (int(empleados['salario'])*0.10), datos) ) )
+    return aumentar_salario_10_porciento
 
 
 def mostrar_datos_con_formato():
-    ...
-
+    '''
+    Muestra sus nombres y su nuevo salario en formato:
+    ["Eva: 1650.0", "Lara: 1980.0"]
+    '''
+    empleados_activos = filtrar_empleados_activos()
+    salario_aumentado = aumentar_salario_10_porciento()
+    empleados_activos_y_salario_aumentado = (list( map( lambda empleado, salario: f'{empleado['nombre']}: {salario}', empleados_activos, salario_aumentado) ) )
+    return empleados_activos_y_salario_aumentado
+    
+    
 
 
 if __name__ == '__main__':
-    ...
+    print(filtrar_empleados_activos())
+    print(aumentar_salario_10_porciento())
+    print(mostrar_datos_con_formato())
---
+++ 01_ejercicios_manipulacion_datos/05_programacion_funcional_aplicada_a_datos/06_analisis_funcional_salarios_remotos.py
@@ -0,0 +1,7 @@
+import os
+os.system('cls')
+
+
+
+if __name__ == '__main__':
+    ...
