86742f404bca8a5ef1b73cafbff3c9084cb932d5
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-05-05 14:11:03 +0200

    Update 01_informe_personalizado_de_videojuegos.py

--- 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/01_informe_personalizado_de_videojuegos.py
+++ 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/01_informe_personalizado_de_videojuegos.py
@@ -1,4 +1,16 @@
 import pandas as pd, os 
 os.system('cls')
 
-df = pd.read_csv('videogames.csv')
+df =  pd.read_csv('videogames.csv')
+
+# Cuál es la consola con más videojuegos en el dataset
+consola_mas_videojuegos = df['console'].value_counts().idxmax()
+print(consola_mas_videojuegos)
+
+# Cuál es el juego más vendido de esa consola
+
+
+# Cuál es el género más frecuente en esa consola
+
+
+# Qué porcentaje de juegos tienen más de 1 millón de ventas (exitosos)
