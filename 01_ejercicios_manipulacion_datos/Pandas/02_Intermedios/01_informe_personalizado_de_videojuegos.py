0e8c1c82b31ea8d264da5621d28849564361a336
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-05-06 14:19:42 +0200

    Update 01_informe_personalizado_de_videojuegos.py

--- 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/01_informe_personalizado_de_videojuegos.py
+++ 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/01_informe_personalizado_de_videojuegos.py
@@ -5,12 +5,14 @@ df =  pd.read_csv('videogames.csv')
 
 # Cuál es la consola con más videojuegos en el dataset
 consola_mas_videojuegos = df['console'].value_counts().idxmax()
-print(consola_mas_videojuegos)
+# print(consola_mas_videojuegos)
 
 # Cuál es el juego más vendido de esa consola
-
+juego_mas_vendido = df[(df['console'] == consola_mas_videojuegos)].sort_values('total_sales', ascending=False).head(1).iloc[0]['title']
+# print(juego_mas_vendido)
 
 # Cuál es el género más frecuente en esa consola
-
+genero_mas_frecuente = df[df['console'] == consola_mas_videojuegos].value_counts()
+print(genero_mas_frecuente)
 
 # Qué porcentaje de juegos tienen más de 1 millón de ventas (exitosos)
