4d6faccb6dfc40778c0e62cd1e7ddffa16c4b76f
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-05-12 11:56:11 +0200

    Update 02_ranking_por_genero.py

--- 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/02_ranking_por_genero.py
+++ 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/02_ranking_por_genero.py
@@ -1,7 +1,6 @@
 import pandas as pd, os 
 os.system('cls')
 
-
 df = pd.read_csv('videogames.csv')
 df = df[['title', 'console', 'genre', 'total_sales']]
 
@@ -11,14 +10,20 @@ lista_generos = []
 for genero in df['genre']:
     if genero not in lista_generos:
         lista_generos.append(genero)
-    
+
+
 genero_input = input('Género: ')
 
 
-if(genero_input == '' or genero_input not in lista_generos):
-    print(f'El género introducido: "{genero_input}" es incorrecto')
-else:
-    df_filtrado = df [df['genre'] == genero_input].sort_values('total_sales', ascending=False)
+if genero_input not in lista_generos or genero_input == '':
+    print('El género introducido es incorrecto')
 
-    for i, (_, fila) in enumerate(df_filtrado.head().iterrows(), start=1):
-        print(f"{i}. {fila['title']} ({fila['total_sales']:.1f}M)")
+else:
+    filas = df[ df['genre'] == genero_input].sort_values('total_sales', ascending=False)
+    print(f'Top 5 videojuegos del género {genero_input}:')
+
+    for i, (_, fila) in enumerate(filas.iterrows(), start=1):
+        if i < 6:
+            print(f'{i}. {fila['title']} ({fila['total_sales']:.1f}M)')
+        else:
+            break
