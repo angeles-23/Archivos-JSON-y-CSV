c24bda992621ca2ecd54549ad564412f57d7df08
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-05-14 10:38:34 +0200

    Update 03_historiograma_generos.py

--- 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/03_historiograma_generos.py
+++ 01_ejercicios_manipulacion_datos/Pandas/02_Intermedios/03_historiograma_generos.py
@@ -1,14 +1,22 @@
-import pandas as pd, os
+import pandas as pd, os 
 os.system('cls')
 
 
 df = pd.read_csv('videogames.csv')
-df = df[['title', 'genre']]
+df = df[['title', 'genre', 'critic_score', 'total_sales']]
 
-generos = df.value_counts('genre')
+generos_agrupados = df.groupby(['genre']).agg(
+    cantidad_juegos = ('title', 'count'),
+    media_ventas = ('total_sales', 'mean'),
+    media_nota_critica = ('critic_score', 'mean')
+)
 
-print('Distribución de videojuegos por género:\n')
 
-for i, j in generos.items():  # items() accede al valor de cada juego
-    bloques = int(j / 10)* '█'
-    print(f'{i.ljust(20)} {bloques} ({j})')
+# generos_agrupados_mayores_5_juegos = generos_agrupados[cantidad_juegos >= 5]
+# print(generos_agrupados_mayores_5_juegos)
+# print(generos_agrupados.sort_values('media_ventas', ascending=False))  # Entre comillas la variable media_ventas
+
+
+# print('--- COMPARATIVA DE GÉNEROS DE VIDEOJUEGOS ---\n')
+# print(f'{'Género'.ljust(20)} {'Juegos'.ljust(20)} {'Ventas medias'.ljust(20)} {'Nota media'.ljust(20)}')
+# print('-'*75)
